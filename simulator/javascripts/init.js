852c250f5da34a2cb5dbab515408c6078a15dda2
VrishabKrishna <krishnavrishab@gmail.com>
2020-05-18 19:53:21 +0530

    lm: add interventions and durations

--- simulator/javascripts/init.js
+++ simulator/javascripts/init.js
@@ -1,16 +1,14 @@
 "use strict";
 
 requirejs.config({
-   
-    baseUrl: 'simulator/javascripts',
+    baseUrl: "simulator/javascripts",
     paths: {
-        controller: 'controller',
-        model:'model'
-    }
+        controller: "controller",
+        model: "model",
+    },
 });
-require(['renderer'],function(renderer){
+require(["renderer"], function (renderer) {
     "use strict";
 
     renderer.set_default_values_html();
-
-})
+});
--- simulator/javascripts/renderer.js
+++ simulator/javascripts/renderer.js
@@ -1,9 +1,6 @@
-define(['model/constants'], function (Constants) {
+define(["model/constants"], function (Constants) {
     "use strict";
     return {
-
-
-
         //To what extent does a family comply with an intervention? 1 = full compliance, 0 = no compliance.
         set_compliance: function () {
             var val = 1;
@@ -54,24 +51,27 @@ define(['model/constants'], function (Constants) {
             document.getElementById("numDays").value = NUM_DAYS;
             document.getElementById("initFrac").value = INIT_FRAC_INFECTED;
             document.getElementById("Incubation").value = 2 * INCUBATION_PERIOD;
-            document.getElementById("asymptomaticMean").value = MEAN_ASYMPTOMATIC_PERIOD;
-            document.getElementById("symptomaticMean").value = MEAN_SYMPTOMATIC_PERIOD;
-            document.getElementById("symtomaticFraction").value = SYMPTOMATIC_FRACTION;
-            document.getElementById("meanHospitalPeriod").value = MEAN_HOSPITAL_REGULAR_PERIOD;
-            document.getElementById("meanICUPeriod").value = MEAN_HOSPITAL_CRITICAL_PERIOD;
+            document.getElementById(
+                "asymptomaticMean"
+            ).value = MEAN_ASYMPTOMATIC_PERIOD;
+            document.getElementById(
+                "symptomaticMean"
+            ).value = MEAN_SYMPTOMATIC_PERIOD;
+            document.getElementById(
+                "symtomaticFraction"
+            ).value = SYMPTOMATIC_FRACTION;
+            document.getElementById(
+                "meanHospitalPeriod"
+            ).value = MEAN_HOSPITAL_REGULAR_PERIOD;
+            document.getElementById(
+                "meanICUPeriod"
+            ).value = MEAN_HOSPITAL_CRITICAL_PERIOD;
             document.getElementById("compliance").value = this.set_compliance();
             document.getElementById("betaHouse").value = BETA_H;
             document.getElementById("betaWork").value = BETA_W;
             document.getElementById("betaCommunity").value = BETA_C;
             document.getElementById("betaSchools").value = BETA_S;
             document.getElementById("betaPT").value = BETA_PT;
-
         },
-
-
-
-
-    }
-
-
+    };
 });
